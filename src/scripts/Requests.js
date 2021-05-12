02dcb180d355f98cac68e18e0827883454c6dbb3
peytonkemp <peyton.kemp@gmail.com>
2021-05-12 10:58:26 -0500

    permanent state and application state

---
+++ src/scripts/Requests.js
@@ -0,0 +1,14 @@
+import { getRequests } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${requests.map()
+        }
+        </ul>
+    `
+
+    return html
+}
